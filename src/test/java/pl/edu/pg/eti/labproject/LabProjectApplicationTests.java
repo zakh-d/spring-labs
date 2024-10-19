f79ea1b3083f4d4ce3e61de668aa8e87609ba42e
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-10-19 19:15:03 +0200

    init: project initialization

---
+++ src/test/java/pl/edu/pg/eti/labproject/LabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.eti.labproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
