1e22dbc755681a035af81bf37897e426860ffbba
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-11-25 20:29:10 +0100

    refactor: project renaming

--- src/main/java/pl/edu/pg/eti/labproject/SimpleWorkoutApplication.java
+++ src/main/java/pl/edu/pg/eti/labproject/SimpleWorkoutApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LabProjectApplication {
+public class SimpleWorkoutApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LabProjectApplication.class, args);
+        SpringApplication.run(SimpleWorkoutApplication.class, args);
     }
 
 }
