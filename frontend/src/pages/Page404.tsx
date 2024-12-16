e8a3d6ad4662dc00d558e2bc5e5b647c2b5c7166
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-12-16 11:35:04 +0100

    feat: added 404 page

---
+++ frontend/src/pages/Page404.tsx
@@ -0,0 +1,11 @@
+import { Container } from "semantic-ui-react";
+
+const Page404 = () => {
+    return (
+        <Container>
+            <h1>404 - Page Not Found</h1>
+        </Container>
+    )
+}
+
+export default Page404;
--- frontend/src/pages/Workout/WorkoutDetailPage.tsx
+++ frontend/src/pages/Workout/WorkoutDetailPage.tsx
@@ -5,20 +5,24 @@ import { Button, Container, Icon } from "semantic-ui-react";
 import WorkoutDetail from "../../components/Workout/WorkoutDetail";
 import { getWorkoutListRoute } from "../../utils/routes";
 import WorkoutPlaceholder from "../../components/Workout/WorkoutPlaceholder";
+import Page404 from "../Page404";
 
 const WorkoutDetailPage = (): ReactElement => {
     const { id } = useParams<{ id: string }>();
     if (!id) {
         return <div>Invalid workout id</div>;
     }
-    const {data, isLoading, isError} = useGetWorkoutQuery(id);
+    const {data, isFetching, error, isError} = useGetWorkoutQuery(id);
 
-    if (isLoading) {
+    if (isFetching) {
         return <Container><WorkoutPlaceholder/></Container>;
     }
 
     if (isError || !data) {
-        return <div>Error...</div>;
+        if (error && error.status === 404) {
+            return <Page404 />;
+        }
+        return <Container>Failed to load workout</Container>;
     }
 
     return (
