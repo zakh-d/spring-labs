42356ab91a6972018dcc1eca1f80e10991d4e1b0
danylozakharchenko <danylo.zakharchenko@gmail.com>
2024-12-16 20:45:22 +0100

    refactor: deleted unused slice

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {workoutSlice} from "./workoutSlice.ts";
 import {workoutApi} from "../api/workout-api.ts";
 
 export const store = configureStore({
     reducer: {
-        workout: workoutSlice.reducer,
         [workoutApi.reducerPath]: workoutApi.reducer
     },
     middleware: (getDefaultMiddleware) => (
